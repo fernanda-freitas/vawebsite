2cd5e3dfac45984278f764146afa5c7b9da817a5
Fernanda Freitas <idea.fernanda@gmail.com>
2023-07-14 11:08:11 +0100

    Add config for responsive font size

--- app/page.js
+++ app/page.js
@@ -1,18 +1,9 @@
 export default function Home() {
   return (
     <>
-      <div className="bg-slate-300 pb-5">grid item</div>
-      <div className="bg-slate-300 pb-5">grid item</div>
-      <div className="bg-slate-300 pb-5">grid item</div>
-      <div className="bg-slate-300 pb-5">grid item</div>
-      <div className="bg-slate-300 pb-5">grid item</div>
-      <div className="bg-slate-300 pb-5">grid item</div>
-      <div className="bg-slate-300 pb-5">grid item</div>
-      <div className="bg-slate-300 pb-5">grid item</div>
-      <div className="bg-slate-300 pb-5">grid item</div>
-      <div className="bg-slate-300 pb-5">grid item</div>
-      <div className="bg-slate-300 pb-5">grid item</div>
-      <div className="bg-slate-300 pb-5">grid item</div>
+      <p class="font-normal text-heading3 md:text-heading2 lg:text-heading1 col-span-12">The quick brown fox</p>
+      <p class="font-normal text-label3 md:text-label2 lg:text-label1 col-span-12">The quick brown fox</p>
+      <p class="font-normal text-p3 md:text-p2 lg:text-label1 col-span-12">The quick brown fox</p>
       {/* <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <h1 className="text-5xl font-loose font-bold underline">Hello, Next.js!</h1>
         <h2>Homepage</h2>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,18 +7,29 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontSize: {
+        'heading1': ['2.625rem', '3.125rem'], // 42px, 50px
+        'heading2': ['1.875rem', '2.313rem'], // 30px, 36px
+        'heading3': ['1.375rem', '26px'], // 22px, 26px
+        'p1': ['1.5rem', '1.75rem'], // 24px, 28px
+        'p2': ['1.375rem', '26px'], // 22px, 26px
+        'p3': ['1.125rem', '1.375rem'], // 18px, 22px
+        'label1': ['1.375rem', '1.375rem'], // 22px, 22px
+        'label2': ['1.25rem', '1.25rem'], // 20px, 20px
+        'label3': ['1rem', '1rem'], // 16px, 16px
+      },
       fontFamily: {
-        normal: ['var(--font-favorit'],
+        normal: ['var(--font-favorit)'],
         loose: ['var(--font-favorit-extended)']
       },
       spacing: {
-        '15': '15px',
-        '25': '25px',
-        '50': '50px',
-        '100': '100px',
-        '150': '150px',
-        '200': '200px',
-      }
+        '15': '0.938rem', // 15px
+        '25': '1.563rem', // 25px
+        '50': '3.125rem', // 50px
+        '100': '6.25rem', // 100px
+        '150': '9.375rem', // 150px
+        '200': '12.5rem', // 200px
+      },
     },
   },
   plugins: [],
