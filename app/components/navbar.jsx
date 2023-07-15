5900d961bd0d7b763a9a0f164c545282ebb21327
Fernanda Freitas <idea.fernanda@gmail.com>
2023-07-15 16:46:16 +0100

    Deploy project

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function Navbar() {
     return (
         <div className="fixed top-0 z-20 flex justify-between w-full py-25 px-2.5 md:px-15 lg:px-5 pb-25 text-white mix-blend-difference">
-            <Link href={`/${process.env.GITHUB_REPOSITORY}`} className="font-normal text-label3 md:text-label1 uppercase">V-a studio</Link>
+            <Link href="/" className="font-normal text-label3 md:text-label1 uppercase">V-a studio</Link>
             <div className="flex space-x-50">
                 <Link href="#" className="font-normal text-label3 md:text-label1 uppercase">Work</Link>
                 <Link href="#" className="font-normal text-label3 md:text-label1 uppercase">About</Link>
