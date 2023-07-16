9cb6f00b6fb31012a3322f87d319e119f0e98b87
Fernanda Freitas <idea.fernanda@gmail.com>
2023-07-16 16:04:18 +0100

    Deploy project

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar() {
     return (
         <div className="fixed top-0 z-20 flex justify-between w-full py-25 px-2.5 md:px-15 lg:px-5 pb-25 text-white mix-blend-difference">
             {/* <Link href="/" className="font-normal text-label3 md:text-label1 uppercase">V-a studio</Link> */}
-            <span onClick={() => route.push("/")} className="font-normal text-label3 md:text-label1 uppercase">V-a studio</span>
+            <span onClick={() => route.push("/vawebsite")} className="font-normal text-label3 md:text-label1 uppercase">V-a studio</span>
             <div className="flex space-x-50">
                 <Link href="#" className="font-normal text-label3 md:text-label1 uppercase">Work</Link>
                 <Link href="#" className="font-normal text-label3 md:text-label1 uppercase">About</Link>
