62b66e812011e7aa8e27b8f07fc0ee6cae9788ae
Fernanda Freitas <idea.fernanda@gmail.com>
2023-07-16 15:46:57 +0100

    Deploy project

--- next.config.js
+++ next.config.js
@@ -16,21 +16,9 @@
 // const withVideos = require('next-videos')
 // module.exports = withVideos(nextConfig);
 
-
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-const repo = 'vawebsite'
-let basePath = ''
-
-if (isGithubActions) {
-  basePath = `/${repo}`
-}
-const assetPrefix = isGithubActions ? `/${repo}/` : ``
-
 const nextConfig = {
   output: 'export',
-  assetPrefix: assetPrefix,
-  basePath: basePath,
+  basePath: '/vawebsite',
   images: { unoptimized: true },
 }
   
