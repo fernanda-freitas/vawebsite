83e27d4052d60028dc66b5a03435647f07a2302e
Fernanda Freitas <idea.fernanda@gmail.com>
2023-07-16 16:07:23 +0100

    Deploy project

--- next.config.js
+++ next.config.js
@@ -1,24 +1,15 @@
 /** @type {import('next').NextConfig} */
 
-// const isGithubActions = process.env.GITHUB_ACTIONS || false
-// const repo = 'vawebsite'
-// let basePath = ''
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+const repo = 'vawebsite'
+let basePath = ''
 
-// if (isGithubActions) {
-//   basePath = `/${repo}`
-// }
-// const nextConfig = {
-//   output: 'export',
-//   basePath: basePath,
-//   images: { unoptimized: true },
-// }
-  
-// const withVideos = require('next-videos')
-// module.exports = withVideos(nextConfig);
-
+if (isGithubActions) {
+  basePath = `/${repo}`
+}
 const nextConfig = {
   output: 'export',
-  basePath: '/vawebsite',
+  basePath: basePath,
   images: { unoptimized: true },
 }
   
