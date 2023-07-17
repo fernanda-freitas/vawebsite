fa6a99b67f896fe071768b72c9281a80468fd022
Fernanda Freitas <idea.fernanda@gmail.com>
2023-07-17 18:08:02 +0100

    Finish responsive menu animations

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -11,25 +11,27 @@ export default function Navbar() {
 
     return (
         <>
-          {open && (
           <motion.div 
             className="absolute top-0 left-0 right-0 bg-black z-20"
-            animate={{bottom: 0}}
-            transition={{duration: 1, ease: "easeInOut"}}
+            animate={open ? {bottom: 0} : {}}
+            transition={open ? {duration: .8, ease: "easeInOut"} : {}}
             >
           </motion.div>
-          )}
           <div className="fixed top-0 z-20 block md:flex justify-between w-full py-25 px-2.5 md:px-15 lg:px-5 pb-25 text-black md:text-white md:mix-blend-difference">
               <div className="flex justify-between items-center -mt-4 z-20">
                   <Link href="/" className={`font-normal text-label3 md:text-label1 uppercase ${open ? 'text-white' : 'text-black'} md:text-white`}>V-a studio</Link>
                   <div onClick={toggleMenu} className="md:hidden flex flex-col space-y-2 py-4 px-2 cursor-pointer">
                       <motion.span 
-                        className={`w-8 h-0.5 ${open? 'bg-white translate-y-p5' : 'bg-black'}`}
-                        animate={ open ? { rotate: '45deg', translateY: '5px'} : {rotate: '0deg', translateY: '0px'}}>
+                        className={`w-8 h-0.5 ${open? 'translate-y-p5 bg-white' : 'bg-black'}`}
+                        animate={ open ? { rotate: '45deg', translateY: '5px'} : {rotate: '0deg', translateY: '0px'}}
+                        transition={{delay: .25}}
+                        >
                       </motion.span>
                       <motion.span 
-                        className={`w-8 h-0.5 ${open? 'bg-white -translate-y-p5' : 'bg-black'}`}
-                        animate={ open ? { rotate: '-45deg', translateY: '-5px'} : {rotate: '0deg', translateY: '0px'}}>
+                        className={`w-8 h-0.5 ${open? '-translate-y-p5 bg-white' : 'bg-black'}`}
+                        animate={ open ? { rotate: '-45deg', translateY: '-5px'} : {rotate: '0deg', translateY: '0px'}}
+                        transition={{delay: .25}}
+                        >
                       </motion.span>
                   </div>
               </div>
@@ -38,34 +40,32 @@ export default function Navbar() {
                   <li><Link href="#" className="text-heading1 font-loose md:text-label1 md:font-normal uppercase">About</Link></li>
                   <li><Link href="#" className="text-heading1 font-loose md:text-label1 md:font-normal uppercase">Contact</Link></li>
               </ul>
-              {open && (
-                  <ul className="flex flex-col md:hidden mt-8 space-y-8 z-20">
-                      <motion.li
-                        className="relative"
-                        initial={{left: "-300px"}}
-                        animate={{ left: "0px" }}
-                        transition={{delay: .3, ease: "easeInOut"}}
-                        >
-                        <Link href="#" className="text-heading1 font-loose md:text-label1 md:font-normal uppercase text-white">Work</Link>
-                      </motion.li>
-                      <motion.li
-                        className="relative"
-                        initial={{left: "-300px"}}
-                        animate={{ left: "0px" }}
-                        transition={{delay: .4, ease: "easeInOut"}}
-                        >
-                        <Link href="#" className="text-heading1 font-loose md:text-label1 md:font-normal uppercase text-white">About</Link>
-                      </motion.li>
-                      <motion.li
-                        className="relative"
-                        initial={{left: "-300px"}}
-                        animate={{ left: "0px" }}
-                        transition={{delay: .48, ease: "easeInOut"}}
-                        >
-                        <Link href="#" className="text-heading1 font-loose md:text-label1 md:font-normal uppercase text-white">Contact</Link>
-                      </motion.li>
-                  </ul>
-              )}
+              <ul className="flex flex-col md:hidden mt-8 space-y-8 z-20">
+                  <motion.li
+                    className="relative"
+                    initial={{left: "-300px"}}
+                    animate={open ? { left: "0px" } : {}}
+                    transition={open ? {delay: .3, ease: "easeInOut"} : {delay: 0}}
+                    >
+                    <Link href="#" className="text-heading1 font-loose md:text-label1 md:font-normal uppercase text-white">Work</Link>
+                  </motion.li>
+                  <motion.li
+                    className="relative"
+                    initial={{left: "-300px"}}
+                    animate={open ? { left: "0px" } : {}}
+                    transition={open ? {delay: .4, ease: "easeInOut"} : {delay: .05}}
+                    >
+                    <Link href="#" className="text-heading1 font-loose md:text-label1 md:font-normal uppercase text-white">About</Link>
+                  </motion.li>
+                  <motion.li
+                    className="relative"
+                    initial={{left: "-300px"}}
+                    animate={open ? { left: "0px" } : {}}
+                    transition={open ? {delay: .48, ease: "easeInOut"} : {delay: .1}}
+                    >
+                    <Link href="#" className="text-heading1 font-loose md:text-label1 md:font-normal uppercase text-white">Contact</Link>
+                  </motion.li>
+              </ul>
           </div>
         </>
     )
